a2dffae98710a8709362c28677c41f6926ebc600
DevelopERdem <baskayaerdem1@gmail.com>
2014-05-08 12:26:06 +0300

    Create Designer.vb

---
+++ Gorevler/Form1/Designer.vb
@@ -0,0 +1,349 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar3 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar4 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar5 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar6 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar7 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar8 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar9 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar10 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown5 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown6 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown7 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown8 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown9 = New System.Windows.Forms.NumericUpDown()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown9, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(0, 234)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(304, 23)
+        Me.ProgressBar1.TabIndex = 0
+        Me.ToolTip1.SetToolTip(Me.ProgressBar1, "0")
+        '
+        'Timer1
+        '
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(186, 0)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(100, 20)
+        Me.ProgressBar2.TabIndex = 2
+        '
+        'ProgressBar3
+        '
+        Me.ProgressBar3.Location = New System.Drawing.Point(186, 26)
+        Me.ProgressBar3.Name = "ProgressBar3"
+        Me.ProgressBar3.Size = New System.Drawing.Size(100, 20)
+        Me.ProgressBar3.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(0, 26)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'ProgressBar4
+        '
+        Me.ProgressBar4.Location = New System.Drawing.Point(186, 52)
+        Me.ProgressBar4.Name = "ProgressBar4"
+        Me.ProgressBar4.Size = New System.Drawing.Size(100, 20)
+        Me.ProgressBar4.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(0, 52)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'ProgressBar5
+        '
+        Me.ProgressBar5.Location = New System.Drawing.Point(186, 78)
+        Me.ProgressBar5.Name = "ProgressBar5"
+        Me.ProgressBar5.Size = New System.Drawing.Size(100, 20)
+        Me.ProgressBar5.TabIndex = 8
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(0, 78)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox4.TabIndex = 7
+        '
+        'ProgressBar6
+        '
+        Me.ProgressBar6.Location = New System.Drawing.Point(186, 104)
+        Me.ProgressBar6.Name = "ProgressBar6"
+        Me.ProgressBar6.Size = New System.Drawing.Size(100, 20)
+        Me.ProgressBar6.TabIndex = 10
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(0, 104)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox5.TabIndex = 9
+        '
+        'ProgressBar7
+        '
+        Me.ProgressBar7.Location = New System.Drawing.Point(186, 130)
+        Me.ProgressBar7.Name = "ProgressBar7"
+        Me.ProgressBar7.Size = New System.Drawing.Size(100, 20)
+        Me.ProgressBar7.TabIndex = 12
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(0, 130)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox6.TabIndex = 11
+        '
+        'ProgressBar8
+        '
+        Me.ProgressBar8.Location = New System.Drawing.Point(186, 156)
+        Me.ProgressBar8.Name = "ProgressBar8"
+        Me.ProgressBar8.Size = New System.Drawing.Size(100, 20)
+        Me.ProgressBar8.TabIndex = 14
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(0, 156)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox7.TabIndex = 13
+        '
+        'ProgressBar9
+        '
+        Me.ProgressBar9.Location = New System.Drawing.Point(186, 182)
+        Me.ProgressBar9.Name = "ProgressBar9"
+        Me.ProgressBar9.Size = New System.Drawing.Size(100, 20)
+        Me.ProgressBar9.TabIndex = 16
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(0, 182)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox8.TabIndex = 15
+        '
+        'ProgressBar10
+        '
+        Me.ProgressBar10.Location = New System.Drawing.Point(186, 208)
+        Me.ProgressBar10.Name = "ProgressBar10"
+        Me.ProgressBar10.Size = New System.Drawing.Size(100, 20)
+        Me.ProgressBar10.TabIndex = 18
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(0, 208)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox9.TabIndex = 17
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(286, 0)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(18, 20)
+        Me.NumericUpDown1.TabIndex = 19
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(286, 26)
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(18, 20)
+        Me.NumericUpDown2.TabIndex = 20
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(286, 52)
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(18, 20)
+        Me.NumericUpDown3.TabIndex = 21
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.Location = New System.Drawing.Point(286, 78)
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(18, 20)
+        Me.NumericUpDown4.TabIndex = 22
+        '
+        'NumericUpDown5
+        '
+        Me.NumericUpDown5.Location = New System.Drawing.Point(286, 104)
+        Me.NumericUpDown5.Name = "NumericUpDown5"
+        Me.NumericUpDown5.Size = New System.Drawing.Size(18, 20)
+        Me.NumericUpDown5.TabIndex = 23
+        '
+        'NumericUpDown6
+        '
+        Me.NumericUpDown6.Location = New System.Drawing.Point(286, 130)
+        Me.NumericUpDown6.Name = "NumericUpDown6"
+        Me.NumericUpDown6.Size = New System.Drawing.Size(18, 20)
+        Me.NumericUpDown6.TabIndex = 24
+        '
+        'NumericUpDown7
+        '
+        Me.NumericUpDown7.Location = New System.Drawing.Point(286, 156)
+        Me.NumericUpDown7.Name = "NumericUpDown7"
+        Me.NumericUpDown7.Size = New System.Drawing.Size(18, 20)
+        Me.NumericUpDown7.TabIndex = 25
+        '
+        'NumericUpDown8
+        '
+        Me.NumericUpDown8.Location = New System.Drawing.Point(286, 182)
+        Me.NumericUpDown8.Name = "NumericUpDown8"
+        Me.NumericUpDown8.Size = New System.Drawing.Size(18, 20)
+        Me.NumericUpDown8.TabIndex = 26
+        '
+        'NumericUpDown9
+        '
+        Me.NumericUpDown9.Location = New System.Drawing.Point(286, 208)
+        Me.NumericUpDown9.Name = "NumericUpDown9"
+        Me.NumericUpDown9.Size = New System.Drawing.Size(18, 20)
+        Me.NumericUpDown9.TabIndex = 27
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 264)
+        Me.Controls.Add(Me.NumericUpDown9)
+        Me.Controls.Add(Me.NumericUpDown8)
+        Me.Controls.Add(Me.NumericUpDown7)
+        Me.Controls.Add(Me.NumericUpDown6)
+        Me.Controls.Add(Me.NumericUpDown5)
+        Me.Controls.Add(Me.NumericUpDown4)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.ProgressBar10)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.ProgressBar9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.ProgressBar8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.ProgressBar7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.ProgressBar6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.ProgressBar5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.ProgressBar4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.ProgressBar3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(320, 300)
+        Me.MinimumSize = New System.Drawing.Size(320, 300)
+        Me.Name = "Form1"
+        Me.Text = "Görevler"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown9, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar2 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar3 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar4 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar5 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar6 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar7 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar8 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar9 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar10 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown3 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown4 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown5 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown6 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown7 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown8 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown9 As System.Windows.Forms.NumericUpDown
+
+End Class
